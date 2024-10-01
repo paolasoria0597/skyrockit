0517093e698100b8a05a7a441823adaa5d3d465f
paolasoria <paola.soria0597@gmail.com>
2024-10-01 11:15:14 -0400

    removed vip-lounge functionality

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const authController = require('./controllers/auth.js');
+const authController = require('./controllers/auth.js'); 
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : '3000'; //
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,9 +17,9 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false })); //middlewear
+app.use(methodOverride('_method')); 
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -34,15 +34,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
 
-app.use('/auth', authController);
+app.use('/auth', authController); 
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
