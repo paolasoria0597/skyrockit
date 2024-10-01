a1fd11ca1ed501e1b9b371f667caef6265de33ca
paolasoria <paola.soria0597@gmail.com>
2024-10-01 15:11:28 -0400

    created a show page

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,14 +6,19 @@ const router = express.Router();
 const User = require('../models/user.js');
 // controllers/applications.js
 
-router.get('/', (req, res) => {
-
-  try  {res.render('applications/index.ejs');
-  } catch (error){
-    console.log(error)
-    res.redirect('/')
-  }
-});
+router.get('/', async (req, res) => {
+    try {
+  
+      const currentUser = await User.findById(req.session.user._id);
+      res.render('applications/index.ejs', {
+        applications: currentUser.applications,
+      })
+  
+    } catch (error) {
+      console.error(error);
+      res.redirect('/')
+    }
+  });
 // controllers/applications.js
 
 router.get('/new', async (req, res) => {
@@ -35,6 +40,22 @@ router.post('/', async (req, res) => {
     } catch (error) {
       // If any errors, log them and redirect back home
       console.log(error);
+      res.redirect('/')
+    }
+  });
+
+  router.get('/:applicationId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Render index.ejs, passing in all of the current user's 
+      // applications as data in the context object. 
+      res.render('applications/show.ejs', {
+        applications: currentUser.applications,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error)
       res.redirect('/')
     }
   });
