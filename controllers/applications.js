70e96a0d233ad4fc65146b76e7ddb7c0bba12f7a
paolasoria <paola.soria0597@gmail.com>
2024-10-01 15:19:49 -0400

    created a delete route

--- controllers/applications.js
+++ controllers/applications.js
@@ -59,6 +59,26 @@ router.post('/', async (req, res) => {
       res.redirect('/')
     }
   });
+  // controllers/applications.js
+
+router.delete('/:applicationId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Use the Mongoose .deleteOne() method to delete 
+      // an application using the id supplied from req.params
+      currentUser.applications.id(req.params.applicationId).deleteOne();
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+  
   
 // we will build out our router logic here
 
